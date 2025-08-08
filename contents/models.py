a9b0d1abb3d3c1cef6496aaacac5c50812afed17
siminju <djsoifd19@naver.com>
2025-08-08 18:05:44 +0900

    models.py수정

--- contents/models.py
+++ contents/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 
-
 class Tags(models.Model):
     tag_name = models.CharField(max_length=5, unique=True)
 
-    def __str__(self):
-        return self.tag_name
 
 class Contents(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -13,5 +10,3 @@ class Contents(models.Model):
     content = models.TextField()
     tag_name = models.ForeignKey(Tags, to_field='tag_name',on_delete=models.PROTECT)
 
-    def __str__(self):
-        return self.title
